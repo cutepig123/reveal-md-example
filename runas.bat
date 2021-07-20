b7ad585632a22c21a23840cb8f8e028a502ac2f1
aeejshe <jshe@asmpt.com>
2021-07-20 13:44:30 +0800

    add theme

--- runas.bat
+++ runas.bat
@@ -1,3 +1,5 @@
 set me=%~dp0%
-node "%me%\node_modules\reveal-md\bin\reveal-md.js"  %1 --preprocessor preproc.js -w
+rem set THEME=--theme white
+node "%me%\node_modules\reveal-md\bin\reveal-md.js"  %1 --preprocessor preproc.js -w %THEME%
+
 pause
